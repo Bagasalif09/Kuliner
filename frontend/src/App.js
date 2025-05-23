d8900f5eefc00e3667ac48d4c4c8813281ad5d39
Bagasalif09 <bagasvirgno09@gmail.com>
2025-05-23 18:06:31 +0700

    Admin e eror

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import AdminDashboard from './pages/AdminDashboard';
+import TenantCrudPage from './pages/TenantCrudPage';
 import HomePage from './pages/HomePage';
 import TenantMenuPage from './pages/TenantMenuPage';
 import './App.css';
@@ -9,7 +11,16 @@ function App() {
     <div className="App">
       <Router>
         <Routes>
+          {/* Halaman utama pengguna umum */}
           <Route path="/" element={<HomePage />} />
+
+          {/* Dashboard admin */}
+          <Route path="/admin" element={<AdminDashboard />} />
+
+          {/* Halaman CRUD tenant untuk admin */}
+          <Route path="/admin/tenant/:tenantId" element={<TenantCrudPage />} />
+
+          {/* Halaman menu tenant untuk pengunjung */}
           <Route path="/tenant/:id" element={<TenantMenuPage />} />
         </Routes>
       </Router>
